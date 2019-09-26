f17851d422e5fe5a971d4dd6afbb28fdface8d16
Blacit <vgilenko@bk.ru>
2019-09-26 15:56:35 +0300

    Last Version

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -15,14 +15,14 @@ import java.util.Properties;
 public class Main {
     private static ArrayList<String> mail = new ArrayList<>(); // Добавляем в список почты
 
-    // ОБЯЗАТЕЛЬНЫЕ ПОЛЯ ДЛЯ ЗАПОЛНЕНИЯ.
-    private static final String themes = "Spamming"; // Заголовок письма
-    private static final String text = "Hello, my friend"; // Текстовое сообщение
-    private static final String username = "ПОЧТА@bk.ru"; // Отправитель mail
-    private static final String password = "PASSWORD"; // Отправитель password
+    // ПОЛЯ ДЛЯ ЗАПОЛНЕНИЯ.
+    private static final String themes = "Spamming"; // Заголовок письма (необязательно)
+    private static final String text = "Hello, my friend"; // Текстовое сообщение (необязательно)
+    private static final String username = "ПОЧТА@bk.ru"; // Отправитель mail (обязательно)
+    private static final String password = "ПАРОЛЬ"; // Отправитель password (обязательно)
     private static final String filepath = "D:\\hi.png"; // Путь к вложению (необязательно)
 
-    public static void main(String[] args) throws MessagingException, ArrayIndexOutOfBoundsException {
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
         String FromMail = "";
 
         //Любая почта, вот для примера:
@@ -34,66 +34,53 @@ public class Main {
         for (int i = 0; i < mail.size(); i++) {
             try {
                 FromMail = mail.get(i).split("\\@")[1];
-            }
-            catch (Exception ignored) {
-            }
-         if (FromMail.equals("mail.ru") || FromMail.equals("bk.ru") || FromMail.equals("gmail.com") || FromMail.equals("yandex.ru") ||
-                 FromMail.equals("inbox.ru") || FromMail.equals("list.ru") || FromMail.equals("yahoo.com") || FromMail.equals("hotmail.com") || FromMail.equals("outlook.com")) {
-         Properties properties = new Properties();
-         properties.put("mail.smtp.host", "smtp.mail.ru");
-         properties.put("mail.smtp.socketFactory.port", "465");
-         properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-         properties.put("mail.smtp.auth", "true");
-         properties.put("mail.smtp.port", "465");
-         send(properties, i, filepath);
-         }
-        }
-    }
-        private static void send(Properties properties, int i, String filepath) {
-            Session session = Session.getDefaultInstance(properties, new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(username, password);
-                }
-            });
-
-            try {
-                Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress(username));
-                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mail.get(i)));
-                message.setSubject(themes);
-                // Отправить текстовое сообщение
-                //message.setText(text);
-                //Файл вложения
-                File file = new File(filepath);
-                if (file.exists() == true) {
-                    MimeMultipart multipart = new MimeMultipart();
-                    MimeBodyPart part1 = new MimeBodyPart();
-                    part1.addHeader("Content-Type", "text/plain; charset=UTF-8");
-                    part1.setDataHandler(new DataHandler(text, "text/plain; charset=\"utf-8\""));
-                    multipart.addBodyPart(part1);
-
-                    MimeBodyPart part2 = new MimeBodyPart();
-                    part2.setFileName(MimeUtility.encodeWord(file.getName()));
-                    part2.setDataHandler(new DataHandler(new FileDataSource(file)));
-                    multipart.addBodyPart(part2);
-                    message.setContent(multipart);
-                    Transport.send(message);
-                }
-                else {
-                    message.setText(text);
-                    Transport.send(message);
-                }
             } catch (Exception ignored) {
             }
+            if (FromMail.equals("mail.ru") || FromMail.equals("bk.ru") || FromMail.equals("gmail.com") || FromMail.equals("yandex.ru") ||
+                    FromMail.equals("inbox.ru") || FromMail.equals("list.ru") || FromMail.equals("yahoo.com") || FromMail.equals("hotmail.com") || FromMail.equals("outlook.com")) {
+                Properties properties = new Properties();
+                properties.put("mail.smtp.host", "smtp.mail.ru");
+                properties.put("mail.smtp.socketFactory.port", "465");
+                properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+                properties.put("mail.smtp.auth", "true");
+                properties.put("mail.smtp.port", "465");
+                send(properties, i, filepath);
+            }
         }
+    }
 
-    private MimeBodyPart createFileAttachment(String filepath)
-            throws MessagingException
-    {
-        MimeBodyPart mbp = new MimeBodyPart();
-        FileDataSource fds = new FileDataSource(filepath);
-        mbp.setDataHandler(new DataHandler(fds));
-        mbp.setFileName(fds.getName());
-        return mbp;
+    private static void send(Properties properties, int i, String filepath) {
+        Session session = Session.getDefaultInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(mail.get(i)));
+            message.setSubject(themes);
+
+            File file = new File(filepath);
+            if (file.exists() == true) {
+                MimeMultipart multipart = new MimeMultipart();
+                MimeBodyPart part1 = new MimeBodyPart();
+                part1.addHeader("Content-Type", "text/plain; charset=UTF-8");
+                part1.setDataHandler(new DataHandler(text, "text/plain; charset=\"utf-8\""));
+                multipart.addBodyPart(part1);
+
+                MimeBodyPart part2 = new MimeBodyPart();
+                part2.setFileName(MimeUtility.encodeWord(file.getName()));
+                part2.setDataHandler(new DataHandler(new FileDataSource(file)));
+                multipart.addBodyPart(part2);
+                message.setContent(multipart);
+                Transport.send(message);
+            } else {
+                message.setText(text);
+                Transport.send(message);
+            }
+        } catch (Exception ignored) {
+        }
     }
 }
